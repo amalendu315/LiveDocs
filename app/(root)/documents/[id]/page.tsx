b951ef81a734d2d9c8bc7022ac11a552e592bbd2
Amalendu Pandey <amalendupandey@Amalendus-MacBook-Pro.local>
2024-07-21 02:05:09 +0530

    liveblocks and editor added

---
+++ app/(root)/documents/[id]/page.tsx
@@ -0,0 +1,14 @@
+
+import CollaborativeRoom from '@/components/collaborative-room';
+
+const Document = () => {
+  return (
+    <main className='flex w-full flex-col items-center'>
+      <CollaborativeRoom 
+
+      />
+    </main>
+  )
+}
+
+export default Document
---
+++ app/provider.tsx
@@ -0,0 +1,18 @@
+"use client"
+import Loader from "@/components/loader";
+import {
+  LiveblocksProvider,
+  ClientSideSuspense,
+} from "@liveblocks/react/suspense";
+
+const Provider = ({children}:{children:React.ReactNode}) => {
+  return (
+    <LiveblocksProvider authEndpoint={"/api/liveblocks-auth"}>
+        <ClientSideSuspense fallback={<Loader />}>
+          {children}
+        </ClientSideSuspense>
+    </LiveblocksProvider>
+  );
+}
+
+export default Provider
